451ea73e82e9b5f16d9764c1386a3212a46c8252
Minhtien863 <142872920+Minhtien863@users.noreply.github.com>
2025-02-11 11:25:39 +0700

    add firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,11 +36,18 @@ android {
 }
 
 dependencies {
+    // Import the Firebase BoM
+    implementation(libs.firebase.bom)
+    implementation (libs.firebase.messaging)
 
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    // https://firebase.google.com/docs/android/setup#available-librarie
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
